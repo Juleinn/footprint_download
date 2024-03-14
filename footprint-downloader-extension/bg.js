0b0b1cad028ac2645efea5b7de7a2c15ee526108
Juleinn <julientartem@hotmail.fr>
2024-03-15 00:00:24 +0100

    Add tab url to file info

--- footprint-downloader-extension/bg.js
+++ footprint-downloader-extension/bg.js
@@ -1,20 +1,32 @@
 chrome.downloads.onChanged.addListener((download_delta) => {
+    console.log({download_delta});
     if(download_delta.filename !== undefined && download_delta !== null){
         current_filename = download_delta.filename.current;
         console.log({current_filename});
 
-        let body =  JSON.stringify({
-            filename: current_filename,
-        })
 
-        fetch("http://localhost:2222", {
-            method: "POST",
-            body: body,
-            headers: {
-                "Content-Length": body.length,
+        chrome.tabs.getSelected(null,function(tab) {
+            console.log({tab});
+            if(tab.url === null && tab.url === undefined) {
+                alert('Failed to retrieve hostname from download tab');
+                return;
             }
-        }).catch((err)=>{
-                console.log({err})
+
+            let body =  JSON.stringify({
+                filename: current_filename,
+                tab_url: tab.url,
             })
+            fetch("http://localhost:2222", {
+                method: "POST",
+                body: body,
+                headers: {
+                    "Content-Length": body.length,
+                }
+            }).catch((err)=>{
+                    console.log({err})
+                })
+        });
+
     }
 })
+
