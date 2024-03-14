846719f790e76080d2da4b12bd2f8327913276e3
Juleinn <julientartem@hotmail.fr>
2024-03-14 23:28:56 +0100

    Add configuration management

--- FootprintDownload.py
+++ FootprintDownload.py
@@ -1,3 +1,5 @@
+import IPython
+import json
 import socket
 import pcbnew
 import os
@@ -19,7 +21,6 @@ def threadfunc():
     while True:
         print("threading hard")
         time.sleep(1)
-
 
 def is_server_running():
     # this is a brittle and hacky way to persist information 
@@ -45,8 +46,53 @@ class FootprintDownload(pcbnew.ActionPlugin):
         self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
         self.description = "A tool for downloading footprints and symbols from distributor sites"
 
+    def read_config(self):
+        pcb_filename = pcbnew.GetBoard().GetFileName()
+        config_filename = os.path.dirname(pcb_filename) + "/.footprintdownloadrc"
+        if not os.path.exists(config_filename):
+            self.config = {
+                "symbol_lib_filename":"",
+                "footprint_lib_directory": "",
+            }
+        else:
+            with open(config_filename, "r") as f:
+                self.config = json.loads(f.read())
+
+    def save_config(self):
+        pcb_filename = pcbnew.GetBoard().GetFileName()
+        config_filename = os.path.dirname(pcb_filename) + "/.footprintdownloadrc"
+        print("writing config : ", self.config)
+        with open(config_filename, "w") as f:
+            f.write(json.dumps(self.config))
+
+    def create_default_config(self):
+        pcb_dir = os.path.dirname(pcbnew.GetBoard().GetFileName())
+        # symbol file : check exist before create
+        symbol_lib_filename = pcb_dir + "/footprint_download.kicad_sym"
+        footprint_lib_directory = pcb_dir + "/footprint_download.pretty"
+        if os.path.exists(symbol_lib_filename):
+            wx.MessageBox(f"{symbol_lib_filename} already exists. Doing nothing")
+        else: 
+            symbol_lib_template = """"(kicad_symbol_lib (version 20211014) (generator FootprintDownloader))"""
+            with open(symbol_lib_filename, "w") as f:
+                f.write(symbol_lib_template)
+
+        if os.path.exists(footprint_lib_directory):
+            wx.MessageBox(f"{footprint_lib_directory} already exists. Doing nothing")
+        else:
+            os.makedirs(footprint_lib_directory)
+
+        # put only the relative paths in config
+        self.config['symbol_lib_filename'] = "/footprint_download.kicad_sym"
+        self.config['footprint_lib_directory'] = "/footprint_download.pretty"
+
     def Run(self):
         dialog = FootprintDownloadDialog(None)
+
+        self.read_config()
+        #IPython.embed(colors="neutral")
+        dialog.symbolLibraryTextbox.SetValue(self.config["symbol_lib_filename"])
+        dialog.footprintLibraryTextbox.SetValue(self.config["footprint_lib_directory"])
 
         def start_server_handler(_):
             if is_server_running():
@@ -62,10 +108,19 @@ class FootprintDownload(pcbnew.ActionPlugin):
             threading.Thread(target=server_threadfunc).start()
             time.sleep(1) # give 1s for the server to start before updating UI accordingly
             pcbnew.footprintdownload_server.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            # update config before serving data
+            pcbnew.footprintdownload_server.config = self.config
             if is_server_running():
                 dialog.serverStatusLabel.SetLabel("Server is running")
                 dialog.startServerButton.Enable(False)
                 dialog.stopServerbutton.Enable(True)
+                # disable configuration changes here 
+                dialog.chooseFootprintLibraryButton.Enable(False)
+                dialog.chooseSymbolLibraryButton.Enable(False)
+                dialog.symbolLibraryTextbox.Enable(False)
+                dialog.footprintLibraryTextbox.Enable(False)
+                dialog.saveConfigButton.Enable(False)
+                dialog.createDefaultConfigButton.Enable(False)
 
         def stop_server_handler(_):
             if not is_server_running():
@@ -80,18 +135,60 @@ class FootprintDownload(pcbnew.ActionPlugin):
             dialog.serverStatusLabel.SetLabel("Server stopped.")
             dialog.startServerButton.Enable(True)
             dialog.stopServerbutton.Enable(False)
+            # disable configuration changes here 
+            dialog.chooseFootprintLibraryButton.Enable(True)
+            dialog.chooseSymbolLibraryButton.Enable(True)
+            dialog.symbolLibraryTextbox.Enable(True)
+            dialog.footprintLibraryTextbox.Enable(True)
+            dialog.saveConfigButton.Enable(True)
+            dialog.createDefaultConfigButton.Enable(True)
+
+        def save_config_handler(_):
+            self.save_config()
+            wx.MessageBox("Config saved")
+
+        def create_default_config_handler(_):
+            self.create_default_config()
+            dialog.symbolLibraryTextbox.SetValue(self.config["symbol_lib_filename"])
+            dialog.footprintLibraryTextbox.SetValue(self.config["footprint_lib_directory"])
+
+            wx.MessageBox("Default config created. Don't forget to save")
+
+        def choose_symbol_library_handler(_):
+            wx.MessageBox("Not implemented")
+
+        def choose_footprint_library_handler(_):
+            wx.MessageBox("Not implemented")
 
         dialog.startServerButton.Bind(wx.EVT_BUTTON, start_server_handler)
         dialog.stopServerbutton.Bind(wx.EVT_BUTTON, stop_server_handler)
+        dialog.saveConfigButton.Bind(wx.EVT_BUTTON, save_config_handler)
+        dialog.createDefaultConfigButton.Bind(wx.EVT_BUTTON, create_default_config_handler)
+        dialog.chooseFootprintLibraryButton.Bind(wx.EVT_BUTTON, choose_footprint_library_handler)
+        dialog.chooseSymbolLibraryButton.Bind(wx.EVT_BUTTON, choose_symbol_library_handler)
 
         if is_server_running():
             dialog.serverStatusLabel.SetLabel("Server is running.")
             dialog.startServerButton.Enable(False)
             dialog.stopServerbutton.Enable(True)
+            # disable configuration changes here 
+            dialog.chooseFootprintLibraryButton.Enable(False)
+            dialog.chooseSymbolLibraryButton.Enable(False)
+            dialog.symbolLibraryTextbox.Enable(False)
+            dialog.footprintLibraryTextbox.Enable(False)
+            dialog.saveConfigButton.Enable(False)
+            dialog.createDefaultConfigButton.Enable(False)
         else:
             dialog.serverStatusLabel.SetLabel("Server is not running")
             dialog.startServerButton.Enable(True)
             dialog.stopServerbutton.Enable(False)
+            # disable configuration changes here 
+            dialog.chooseFootprintLibraryButton.Enable(True)
+            dialog.chooseSymbolLibraryButton.Enable(True)
+            dialog.symbolLibraryTextbox.Enable(True)
+            dialog.footprintLibraryTextbox.Enable(True)
+            dialog.saveConfigButton.Enable(True)
+            dialog.createDefaultConfigButton.Enable(True)
 
         dialog.Show()
 
--- FootprintDownloadDialog.py
+++ FootprintDownloadDialog.py
@@ -14,10 +14,10 @@ import wx.xrc
 ## Class FootprintDownloadDialogFrame
 ###########################################################################
 
-class FootprintDownloadDialogFrame ( wx.Frame ):
+class FootprintDownloadDialogFrame ( wx.Dialog ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 755,368 ), style = wx.DEFAULT_DIALOG_STYLE )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
@@ -37,7 +37,53 @@ class FootprintDownloadDialogFrame ( wx.Frame ):
 		bSizer2.Add( self.stopServerbutton, 0, wx.ALL, 5 )
 
 
-		bSizer1.Add( bSizer2, 1, wx.EXPAND, 5 )
+		bSizer1.Add( bSizer2, 0, wx.EXPAND, 5 )
+
+		self.m_staticline1 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+		bSizer1.Add( self.m_staticline1, 0, wx.EXPAND |wx.ALL, 5 )
+
+		bSizer21 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"symbol library   :", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
+
+		bSizer21.Add( self.m_staticText2, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+		self.symbolLibraryTextbox = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer21.Add( self.symbolLibraryTextbox, 1, wx.ALL, 5 )
+
+		self.chooseSymbolLibraryButton = wx.Button( self, wx.ID_ANY, u"Choose", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer21.Add( self.chooseSymbolLibraryButton, 0, wx.ALL, 5 )
+
+
+		bSizer1.Add( bSizer21, 0, wx.EXPAND, 5 )
+
+		bSizer211 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.m_staticText21 = wx.StaticText( self, wx.ID_ANY, u"footprint library :", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText21.Wrap( -1 )
+
+		bSizer211.Add( self.m_staticText21, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+		self.footprintLibraryTextbox = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer211.Add( self.footprintLibraryTextbox, 1, wx.ALL, 5 )
+
+		self.chooseFootprintLibraryButton = wx.Button( self, wx.ID_ANY, u"Choose", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer211.Add( self.chooseFootprintLibraryButton, 0, wx.ALL, 5 )
+
+
+		bSizer1.Add( bSizer211, 0, wx.EXPAND, 5 )
+
+		bSizer2111 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.createDefaultConfigButton = wx.Button( self, wx.ID_ANY, u"Create Default", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer2111.Add( self.createDefaultConfigButton, 0, wx.ALL, 5 )
+
+		self.saveConfigButton = wx.Button( self, wx.ID_ANY, u"Save Config", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer2111.Add( self.saveConfigButton, 0, wx.ALL, 5 )
+
+
+		bSizer1.Add( bSizer2111, 0, wx.EXPAND, 5 )
 
 
 		self.SetSizer( bSizer1 )
